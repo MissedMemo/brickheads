e329a70eae81501532ec634014146fec371d913f
Andrew Lloyd <MissedMemo@GMail.com>
2016-03-11 08:38:51 -0800

    fixed lack of Chrome support for vw units in letter-spacing

--- public/js/login-controller.js
+++ public/js/login-controller.js
@@ -1,6 +1,8 @@
 angular.module('app')
   .controller( 'LoginController', [ '$scope', '$state', function( $scope, $state ) {
 
+     // convert to use local storage (angular-local-storage.js)
+     // to enable value to survive page refresh...
     $scope.isLoggedIn = false;
 
     $scope.logInOrOut = function() {
