e7cfdf7b43b80ee6328728cb4f45697ae9e6e0a6
Andrew Lloyd <MissedMemo@GMail.com>
2016-03-11 20:38:16 -0800

    initializing nested taskView AND $scope.currentTask, works on page refresh

--- public/js/companies-controller.js
+++ public/js/companies-controller.js
@@ -1,10 +1,9 @@
 angular.module('app')
-  .controller( 'CompaniesController', [ '$scope', 'CompanyFactory', function( $scope, CompanyFactory ) {
+  .controller( 'CompaniesController', [ '$scope', '$timeout', 'CompanyFactory', function( $scope, $timeout, CompanyFactory ) {
 
     $scope.companies = CompanyFactory.getAll();
     $scope.selection = $scope.companies[0];
-    $scope.currentTask = 'research';
-    
+        
     $scope.select = function( company ) {
       $scope.selection = company;
     };
@@ -12,5 +11,18 @@ angular.module('app')
     $scope.setTask = function( task ) {
       $scope.currentTask = task;
     };
+
+    // HACK: force click event on #defaultTask DOM element
+    // to init nested view AND $scope.currentTask
+    
+    var privateInit = function () {
+      $timeout( function() {
+        var node = document.querySelector('#defaultTask');
+        angular.element(node).triggerHandler('click');
+      }, 100);
+    };
+
+    privateInit();
+    
 
   }]);
